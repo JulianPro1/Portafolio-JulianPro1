a1aeee920669f7178075e560b16039cf00d3331b
JulianPro1 <jjosealejos07@gmail.com>
2024-07-31 18:48:58 -0400

    modificacion de rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,10 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
 {
+    path:'',
+    loadComponent: () => import('./presentation/presentation.component')
+},
+{
     path:'presentation', 
     loadComponent: () => import('./presentation/presentation.component')
 },
